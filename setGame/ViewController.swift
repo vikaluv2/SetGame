7b018cc03348d5f12f6a34e7f1fd8af691662c20
Vika Toloke <veetoloke@gmail.com>
2019-05-28 08:41:52 -1000

    Lec 5

--- setGame/ViewController.swift
+++ setGame/ViewController.swift
@@ -1,20 +1,15 @@
-//
-//  ViewController.swift
-//  setGame
-//
-//  Created by Vika Maopa Toloke on 5/22/19.
-//  Copyright © 2019 Vika Maopa Toloke. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var deck = PlayingCardDeck()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        for _ in 1...10 {
+            if let card = deck.draw() {
+                print("\(card)")
+            }
+        }
     }
-
-
 }
-
