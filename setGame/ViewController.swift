80217c3072a4b871ea39017f15999f284eb283b5
Vika Toloke <veetoloke@gmail.com>
2019-05-29 12:44:02 -1000

    -buttons to storyboard

--- setGame/ViewController.swift
+++ setGame/ViewController.swift
@@ -6,18 +6,18 @@
 //  Copyright © 2019 Vika Maopa Toloke. All rights reserved.
 //
 
-
 import UIKit
 
 class ViewController: UIViewController {
     
     @IBOutlet private var setCardButtons: [UIButton]!
     
-    @IBOutlet weak var dealButton: UIButton!
+    @IBOutlet weak var dealThreeMoreCardsButton: UIButton!
     
     @IBOutlet weak var scoreLabel: UILabel!
     
     let setGame = SetGame()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,16 +25,17 @@ class ViewController: UIViewController {
         updateViewFromModel()
     }
     
-    func resetButtons() { //attributes of the buttons
+    func resetButtons() { //attributes of buttons that will come up
         for buttonIndex in setCardButtons.indices {
             let button = setCardButtons[buttonIndex]
             button.deselect()
-            button.titleLabel?.font = UIFont.systemFont(ofSize: 28.0) //size of chars
-            button.backgroundColor = #colorLiteral(red: 1, green: 0.5768225789, blue: 0, alpha: 0) //makes card (color) there but its invisible
-            button.layer.borderColor =  #colorLiteral(red: 1, green: 0.5768225789, blue: 0, alpha: 0) //border is clear too
+            button.titleLabel?.font = UIFont.systemFont(ofSize: 28.0) //part of hint
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5768225789, blue: 0, alpha: 0)
+            button.layer.borderColor =  #colorLiteral(red: 1, green: 0.5768225789, blue: 0, alpha: 0) //its there but you csnt see it until you draw the card
             button.setAttributedTitle(nil, for: UIControl.State.normal)
         }
     }
+    
     
     @IBAction private func selectCard(_ sender: UIButton) {
         if let cardIndex = setCardButtons.index(of: sender) {
@@ -46,27 +47,27 @@ class ViewController: UIViewController {
     }
     
     @IBAction func newGame() {
-        dealButton.isEnabled = true
+        dealThreeMoreCardsButton.isEnabled = true
         setGame.newGame()
         resetButtons()
         updateViewFromModel()
     }
     
-    @IBAction func addThreeCards() {
-        setGame.addCards(numberOfCardsToSelect: 3)
+    @IBAction func addThreeNewCards() {
+        setGame.addCards(numberOfCardsToSelect: 3) //adds 3 cards at a time else crash
         updateViewFromModel()
-        if setGame.cardsInGame.count >= 24 {
-            dealButton.isEnabled = false
+        if setGame.cardsInGame.count >= 24 { //24 cards total to display but the deck is
+            dealThreeMoreCardsButton.isEnabled = false //cant
         }
     }
-    
+
     private func updateViewFromModel() {
         var cardButtonIndex = 0
         resetButtons()
-        scoreLabel.text = "Score: \(setGame.score)" //keeps track of the score
+        scoreLabel.text = "Score: \(setGame.score)"
         for card in setGame.cardsInGame {
             let button = setCardButtons[cardButtonIndex]
-            ButtonRender.chosenCard(cardToChoose: card,
+            Button.chosenCard(cardToChoose: card,
                                     onButton: button,
                                     selectButton: setGame.cardIsSelected(card: card),
                                     isSet: setGame.isSet())
@@ -75,7 +76,7 @@ class ViewController: UIViewController {
     }
 }
 
-
+//copied from lec 2 concentration
 extension Int {
     func arc4Random() -> Int {
         if self > 0 {
