71339fd84258dffea63ac2b3dab1639e87afd7a3
Vika Toloke <veetoloke@gmail.com>
2019-05-29 12:45:13 -1000

    attributes on cards. hint:. Note that a UIButton’s title and its attributedTitle can both be set separately (attributedTitle takes precedence if set) so, for example, if you wanted a UIButton to have no title at all, you’d want to set both of those to nil.

--- setGame/PlayingCard.swift
+++ setGame/PlayingCard.swift
@@ -4,8 +4,8 @@
 //
 //  Created by Vika Maopa Toloke on 5/22/19.
 //  Copyright © 2019 Vika Maopa Toloke. All rights reserved.
-//
 
+//Card Attributes
 import Foundation
 
 struct Card : Equatable {
@@ -13,14 +13,46 @@ struct Card : Equatable {
     static func ==(lhs: Card, rhs: Card) -> Bool {
         return lhs.cardColor == rhs.cardColor &&
             lhs.cardNumber == rhs.cardNumber &&
-            lhs.cardShading == rhs.cardShading &&
+            lhs.cardType == rhs.cardType &&
             lhs.cardSymbol == rhs.cardSymbol
     }
     
     let cardColor: CardColor
     let cardSymbol: CardSymbol
     let cardNumber: CardNumber
-    let cardShading: CardShading
+    let cardType: CardType
+}
+//enums are only value types
+enum CardColor {
+    case red
+    case green
+    case purple
+    
+    static let allValues = [red, green, purple]
+}
+
+enum CardSymbol {
+    case diamond
+    case squiggle
+    case oval
+    
+    static let allValues = [diamond, squiggle, oval]
+}
+
+enum CardNumber {
+    case one
+    case two
+    case three
+    
+    static let allValues = [one, two, three]
+}
+
+enum CardType {
+    case solid
+    case striped
+    case open
+    
+    static let allValues = [solid, striped, open]
 }
 
 
--- setGame/SetGame.swift
+++ setGame/SetGame.swift
@@ -5,7 +5,7 @@
 //  Created by Vika Maopa Toloke on 5/22/19.
 //  Copyright © 2019 Vika Maopa Toloke. All rights reserved.
 //
-
+//
 
 import Foundation
 
@@ -20,21 +20,36 @@ class SetGame {
         newGame()
     }
     
-    //This will reset the game -
+    //This will reset the game
     func newGame() {
         score = 0
         availableCards.removeAll() //removes all cards
         cardsInGame.removeAll()
         selectedCards.removeAll()
+        
+        generateAllCardCombinations()
         addCards(numberOfCardsToSelect: 12)
         
     }
     
+    private func generateAllCardCombinations() {
+        for color in CardColor.allValues {
+            for symbol in CardSymbol.allValues {
+                for number in CardNumber.allValues {
+                    for shading in CardType.allValues {
+                        let card = Card(cardColor: color, cardSymbol: symbol, cardNumber: number, cardType: shading)
+                        availableCards.append(card)
+                    }
+                }
+            }
+        }
+    }
+    
 
     
-    private func addCard() {
+    private func addCard() { //makes it random the added cards
         let selectedCard = availableCards.remove(at: availableCards.count.arc4Random())
-        cardsInGame.append(selectedCard)
+        cardsInGame.append(selectedCard) //appends for
     }
     
     func addCards(numberOfCardsToSelect numberOfCards: Int) {
@@ -44,23 +59,66 @@ class SetGame {
     }
     
     func cardIsSelected(card: Card) -> Bool {
-        return selectedCards.index(of: card) != nil
+        return selectedCards.index(of: card) != nil //not null bc selected
     }
     
+    //THIS IS HOW THE GAME works
     func isSet() -> Bool {
+        if selectedCards.count != 3 { //not 3, dont do anything
+            return false
+        }
         
-        if selectedCards.count != 3 {
+        //scenario 1
+        if selectedCards[0].cardColor == selectedCards[1].cardColor {
+            if selectedCards[0].cardColor != selectedCards[2].cardColor {
+                return false
+            }
+        } else if selectedCards[1].cardColor == selectedCards[2].cardColor {
+            return false
+        } else if (selectedCards[0].cardColor == selectedCards[2].cardColor) {
+            return false
+        }
+        //
+        if selectedCards[0].cardNumber == selectedCards[1].cardNumber {
+            if selectedCards[0].cardNumber != selectedCards[2].cardNumber {
+                return false
+            }
+        } else if selectedCards[1].cardNumber == selectedCards[2].cardNumber {
+            return false
+        } else if (selectedCards[0].cardNumber == selectedCards[2].cardNumber) {
+            return false
+        }
+        //
+        if selectedCards[0].cardType == selectedCards[1].cardType {
+            if selectedCards[0].cardType != selectedCards[2].cardType {
+                return false
+            }
+        } else if selectedCards[1].cardType == selectedCards[2].cardType {
+            return false
+        } else if (selectedCards[0].cardType == selectedCards[2].cardType) {
+            return false
+        }
+        //
+        if selectedCards[0].cardSymbol == selectedCards[1].cardSymbol {
+            if selectedCards[0].cardSymbol != selectedCards[2].cardSymbol {
+                return false
+            }
+        } else if selectedCards[1].cardSymbol == selectedCards[2].cardSymbol {
+            return false
+        } else if (selectedCards[0].cardSymbol == selectedCards[2].cardSymbol) {
             return false
         }
         
         return true
     }
     
+    
+    //selecting the card
     func select(card: Card) {
-        if selectedCards.count == 3 && isSet() {
+        if selectedCards.count == 3 && isSet() { //is a match
             selectedCards.forEach {
                 if let selectedCardInGameIndex = cardsInGame.index(of: $0) {
-                    cardsInGame.remove(at: selectedCardInGameIndex)
+                    cardsInGame.remove(at: selectedCardInGameIndex) //removes the matched cards
                     if availableCards.count > 0 {
                         let selectedCard = availableCards.remove(at: availableCards.count.arc4Random())
                         cardsInGame.insert(selectedCard, at: selectedCardInGameIndex)
@@ -74,8 +132,8 @@ class SetGame {
             score -= 1
         }
         
-        if let cardToSelect = selectedCards.index(of: card) {
-            selectedCards.remove(at: cardToSelect) //removes it when selected if not needed
+        if let card2Select = selectedCards.index(of: card) {
+            selectedCards.remove(at: card2Select) //removes it when selected if not needed
         } else {
             selectedCards.append(card)
         }
